adc1035f47c4b6c8c2cfa24d02249e995b1aabf3
adityamawa <aditya.05mawa@gmail.com>
2025-02-17 18:15:38 +0530

    Re-adding updated Files

---
+++ Server/models/profile.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const profileSchema = new mongoose.Schema({
+    gender:{
+        type:String,
+    },
+    dateOfBirth:{
+        type:String,
+    },
+    about:{
+        type:String,
+        trim:true,
+    },
+    contactNumber:{
+        type:String,
+        trim:true,
+    }
+})
+
+const Profile = mongoose.model("Profile",profileSchema)
+
+export default Profile;
